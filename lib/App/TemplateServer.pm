a3880b09127c63add51e202a64b41865f936c6e3
Jonathan Rockway <jon@jrock.us>
2007-11-29 13:15:41 -0600

    cleanup after catalystx-starter

--- lib/App/TemplateServer.pm
+++ lib/App/TemplateServer.pm
@@ -2,9 +2,11 @@ package App::TemplateServer;
 use strict;
 use warnings;
 
+our $VERSION = '0.01';
+
 =head1 NAME
 
-App::TemplateServer - 
+App::TemplateServer - application to serve processed templates
 
 =cut
 
--- t/lib/TestApp.pm
+++
@@ -1,9 +0,0 @@
-package TestApp;
-use strict;
-use warnings;
-
-use Catalyst;
-
-__PACKAGE__->setup;
-
-1;
