d548b752ed3d6327cb377b2b9661f6fb32675bdb
Jonathan Rockway <jon@jrock.us>
2007-12-08 23:03:35 -0600

    doc provider role

--- lib/App/TemplateServer/Provider.pm
+++ lib/App/TemplateServer/Provider.pm
@@ -13,3 +13,43 @@ requires 'list_templates';
 requires 'render_template';
 
 1;
+
+__END__
+
+=head1 NAME
+
+App::TemplateServer::Provider - role that a Provider should do
+
+=head1 DESCRIPTION
+
+Template systems are interfaced with App::TemplateServer with this
+role.  The template server will call the methods required by this role
+to provider its functionality.
+
+=head1 ATTRIBUTES
+
+This role provides the following attributes:
+
+=head2 docroot
+
+This is the directory where your templates that you are "providing"
+live.  It is required.
+
+=head1 REQUIRED METHODS
+
+You need to implement these:
+
+=head2 list_templates
+
+Returns a list of strings representing template names.
+
+=head2 render_template($template, $context)
+
+Return the rendered text of the template named by C<$template>.  If
+C<$template> can't be rendered, throw an exception.  C<$context> is
+the L<App::TemplateServer::Context|App::TemplateServer::Context>
+object for the request.
+
+=head1 SEE ALSO
+
+L<App::TemplateServer|App::TemplateServer>
